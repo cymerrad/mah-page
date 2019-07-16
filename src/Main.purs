193b52db76c8af3f81d27740cbe6549930589ca6
Radosław Cymer <cymerrad@gmail.com>
2019-07-16 22:52:38 +0200

    Two circles moving; purescript-web-html sucks

--- src/Main.purs
+++ src/Main.purs
@@ -1,22 +1,34 @@
 module Main where
 
 import Prelude
+
 import Color (white, hsl)
+import Control.Monad.Error.Class (withResource)
 import Data.Array (take)
 import Data.Foldable (foldMap)
 import Data.Int (floor, toNumber)
+import Data.Map as Map
 import Data.Maybe (Maybe(..), fromJust)
+import Data.Tuple (Tuple(..))
 import Data.Unfoldable (replicateA)
 import Effect (Effect)
 import Effect.Random (random, randomRange)
 import Flare (UI, buttons, foldp, intSlider_, lift, runFlareWith)
 import Flare.Drawing (Drawing, circle, fillColor, filled)
-import Graphics.Canvas (Context2D, getCanvasElementById, getContext2D, getCanvasWidth, getCanvasHeight, clearRect)
+import Graphics.Canvas (Context2D, clearRect, getCanvasElementById, getCanvasHeight, getCanvasWidth, getContext2D, moveTo)
 import Graphics.Drawing (Point, render)
 import Math (Radians, log, cos, sin, sqrt, pi)
 import Partial.Unsafe (unsafePartial)
-import Signal.DOM (animationFrame)
+import Signal.DOM (CoordinatePair, animationFrame, mousePos)
 import Signal.Time (Time)
+import Unsafe.Coerce (unsafeCoerce)
+import Web.DOM.Document (doctype)
+import Web.DOM.Internal.Types (Element)
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (HTMLDocument, HTMLElement, window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.HTMLElement (offsetTop, offsetLeft)
+import Web.HTML.Window (document)
 
 type Object
   = { id :: String
@@ -31,17 +43,27 @@ type Model
   = { ctx :: Context2D
     , width :: Number
     , height :: Number
-    , objects :: Array Object
+    , baseX :: Number
+    , baseY :: Number
+    , objects :: Map.Map String Object
     , time :: Time
     }
 
-startObj :: Effect Object
-startObj = pure { id: "object", css: "", x: 0.0, y: 0.0, vx: 0.0, vy: 0.0 }
+startObjects :: Number -> Number -> Effect (Map.Map String Object)
+startObjects width height =
+  pure
+    $ Map.fromFoldable
+        [ Tuple "bait" { id: "bait", css: "", x: 0.0, y: 0.0, vx: 0.0, vy: 0.0 }
+        , Tuple "character" { id: "character", css: "", x: (width / 2.0), y: height, vx: 0.0, vy: 0.0 }
+        ]
 
-move :: Object -> Time -> Object
-move o dt = o -- TODO spreading only some properties of the object?
+moveObj :: Object -> Time -> Object
+moveObj o dt = o -- TODO spreading only some properties of the object?
 
-drawObjects :: Array Object -> Time -> Drawing
+moveObjTo :: Object -> Number -> Number -> Object
+moveObjTo o n_x n_y = o { x = n_x, y = n_y }
+
+drawObjects :: Map.Map String Object -> Time -> Drawing
 drawObjects objects time = foldMap makeCircle objects
   where
   makeCircle o = filledCircle o.x o.y 23.0
@@ -56,26 +78,52 @@ controller model = do
   clearRect model.ctx { x: 0.0, y: 0.0, width: model.width, height: model.height }
   render model.ctx $ drawObjects model.objects model.time
 
--- data Action
---   = Start
---   | Reset
--- label :: Action -> String
--- label Start = "start"
--- label Reset = "reset"
--- isAction :: Maybe Action -> Action -> Boolean
--- isAction (Just a) b = label a == label b
--- isAction Nothing _ = false
+-- view updates
+mouseMoved :: CoordinatePair -> Model -> Model
+mouseMoved new_coord m =
+  m
+    { objects = Map.update (\v -> pure $ moveObjTo v (toNumber new_coord.x - m.baseX) (toNumber new_coord.y - m.baseY)) "bait" m.objects
+    }
+
+sliderMoved :: Int -> Model -> Model
+sliderMoved new_pos m =
+  m
+    { objects = Map.update (\v -> pure $ moveObjTo v (toNumber new_pos) v.y) "character" m.objects
+    }
+
+timePassed :: Number -> Model -> Model
+timePassed new_time m = m { time = new_time }
+
+--   state m_coord n time =
+--     model
+--       { objects = map (\o -> o { x = (toNumber m_coord.x), y = (toNumber m_coord.y) }) model.objects
+--       , time = time
+--       }
 view :: Model -> UI Model
 view model =
-  -- mouse events here
-  state <$> intSlider_ 0 (floor model.width) (floor model.width)
-    <*> lift animationFrame
+  state <$> lift animationFrame
+    <*> lift mousePos
+    <*> intSlider_ 0 (floor model.width) (floor model.width)
+    <*> pure model
   where
-  state n time =
-    model
-      { objects = map (\o -> o { x = (toNumber n) }) model.objects
-      , time = time
-      }
+  state time coord n =
+    timePassed time
+      <<< mouseMoved coord
+      <<< sliderMoved n
+
+-- | purescript-web-html SUCKS ASS
+toHTMLElement :: Element -> HTMLElement
+toHTMLElement = unsafeCoerce
+
+getHTMLElementbyId :: String -> Effect HTMLElement
+getHTMLElementbyId id = do
+  win <- window
+  doc <- document win
+  let docEl =  toNonElementParentNode doc
+  wut <- getElementById id docEl
+  let kurwa = unsafePartial $ fromJust wut
+  pure $ toHTMLElement kurwa
+
 
 main :: Effect Unit
 -- main = runFlareDrawing "controls" "output" ui
@@ -86,12 +134,17 @@ main = do
   ctx <- getContext2D pcanvas
   width <- getCanvasWidth pcanvas
   height <- getCanvasHeight pcanvas
-  objects <- replicateA 1 $ startObj
+  canvasEl <- getHTMLElementbyId "output"
+  baseX <- offsetLeft canvasEl
+  baseY <- offsetTop canvasEl
+  objects <- startObjects width height
   runFlareWith "controls" controller
     $ view
         { ctx: ctx
         , width: width
         , height: height
+        , baseX: baseX
+        , baseY: baseY
         , objects: objects
         , time: 0.0
         }
