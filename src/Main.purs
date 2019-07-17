9313ab42386f122988b73239e2de4d127d4cd55e
Radosław Cymer <cymerrad@gmail.com>
2019-07-17 17:29:25 +0200

    Migrate from Flare to Signal only

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,16 @@
 module Main where
 
+import MahUtils.Vector
 import Prelude
 import Color (hsl)
 import Data.Foldable (foldMap)
-import Data.Int (floor, toNumber)
+import Data.Int (toNumber)
 import Data.Map as Map
+import Data.Maybe (Maybe)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Flare (UI, intSlider_, lift, runFlareWith)
+import Effect.Console (log)
+import Flare (UI, lift, runFlareWith)
 import Flare.Drawing (Drawing, circle, fillColor, filled)
 import Graphics.Canvas (Context2D, clearRect, getCanvasHeight, getCanvasWidth, getContext2D)
 import Graphics.Drawing (render)
@@ -19,52 +22,70 @@ import Web.HTML.HTMLElement (offsetLeft, offsetTop)
 type Object
   = { id :: String
     , hueVal :: Number -- some sprite definition
-    , x :: Number
-    , y :: Number
-    , vx :: Number
-    , vy :: Number
+    , position :: Vector
+    , velocity :: Vector
     }
 
 type ObjectMap
   = Map.Map String Object
 
+type GameEnvironment
+  = { objects :: ObjectMap }
+
 type Model
   = { ctx :: Context2D
-    , width :: Number
-    , height :: Number
-    , baseX :: Number
-    , baseY :: Number
-    , pX :: Number
-    , pY :: Number
-    , objects :: Map.Map String Object
+    , canvasSize :: Vector
+    , canvasPos :: Vector
+    , pointerPos :: Vector
+    , env :: GameEnvironment
     , time :: Time
     , dTime :: Number
+    , ticks :: Int
+    }
+
+updateObjects :: Model -> (Object -> Maybe Object) -> String -> Model
+updateObjects model updateFunc key =
+  model
+    { env
+      { objects = Map.update updateFunc key model.env.objects
+      }
+    }
+
+updateObjectsAll :: Model -> (Object -> Maybe Object) -> Model
+updateObjectsAll model updateFunc =
+  model
+    { env
+      { objects = Map.mapMaybe updateFunc model.env.objects
+      }
     }
 
 movementFactor :: Number
-movementFactor = 0.1
+movementFactor = 0.5
 
 waterLevel :: Model -> Number
-waterLevel m = m.height
+waterLevel m = m.canvasSize.y
 
 startObjects :: Model -> Effect ObjectMap
 startObjects m =
   pure
     $ Map.fromFoldable
-        [ Tuple "bait" { id: "bait", hueVal: 0.0, x: 0.0, y: 0.0, vx: 0.0, vy: 0.0 }
-        , Tuple "fish" { id: "fish", hueVal: 200.0, x: (m.width / 2.0), y: (waterLevel m), vx: 0.0, vy: 0.0 }
+        [ Tuple "bait" { id: "bait", hueVal: 0.0, position: { x: 0.0, y: 0.0 }, velocity: { x: 0.0, y: 0.0 } }
+        , Tuple "fish" { id: "fish", hueVal: 200.0, position: { x: (m.canvasSize.x / 2.0), y: (waterLevel m) }, velocity: { x: 0.0, y: 0.0 } }
         ]
 
 moveObj :: Object -> Time -> Object
 moveObj o dt = o -- TODO spreading only some properties of the object?
 
-moveObjTo :: Object -> Number -> Number -> Object
-moveObjTo o n_x n_y = o { x = n_x, y = n_y }
+setObjPos :: Object -> Number -> Number -> Object
+setObjPos o n_x n_y = o { position = { x: n_x, y: n_y } }
 
-drawObjects :: ObjectMap -> Time -> Drawing
-drawObjects objects time = foldMap makeCircle objects
+setObjVel :: Object -> Number -> Number -> Object
+setObjVel o n_x n_y = o { velocity = { x: n_x, y: n_y } }
+
+drawObjects :: GameEnvironment -> Time -> Drawing
+drawObjects env time = foldMap makeCircle env.objects
   where
-  makeCircle o = filledCircle o.hueVal o.x o.y 23.0
+  makeCircle o = filledCircle o.hueVal o.position.x o.position.y 23.0
 
 filledCircle :: Number -> Number -> Number -> Number -> Drawing
 filledCircle hue_val pos_x pos_y radius = filled (fillColor col) (circle pos_x pos_y radius)
@@ -74,17 +95,11 @@ filledCircle hue_val pos_x pos_y radius = filled (fillColor col) (circle pos_x p
 -- TODO rod is still equal to bait, in future bait will follow rod
 -- instantaneous move
 rodMoves :: Model -> Model
-rodMoves m =
-  m
-    { objects = Map.update moveToPointer "bait" m.objects
-    }
+rodMoves m = updateObjects m moveToPointer "bait"
   where
   moveToPointer obj =
     pure
-      $ obj
-          { x = m.pX
-          , y = m.pY
-          }
+      $ setObjPos obj m.pointerPos.x m.pointerPos.y
 
 -- aplies momentum to rod
 baitFollowsRod :: Model -> Model
@@ -94,67 +109,50 @@ baitFollowsRod = identity
 fishFollowsBait :: Model -> Model
 fishFollowsBait m =
   let
-    baitM = Map.lookup "bait" m.objects
+    baitM = Map.lookup "bait" m.env.objects
   in
-    m
-      { objects = Map.update (fishFollow baitM) "fish" m.objects
-      }
+    updateObjects m (fishFollow baitM) "fish"
   where
   fishFollow bM f = fishFollowSimple <$> bM <*> pure f
     where
     fishFollowSimple :: Object -> Object -> Object
-    fishFollowSimple bait fish =
-      fish
-        { vx = bait.x - fish.x
-        , vy = bait.y - fish.y
-        }
+    fishFollowSimple bait fish = setObjVel fish (bait.position.x - fish.position.x) (bait.position.y - fish.position.y)
 
 -- execute momentum of objects
 objectsMove :: Model -> Model
-objectsMove m =
-  m
-    { objects = Map.mapMaybe momentum m.objects
-    }
+objectsMove m = updateObjectsAll m momentum
   where
-  momentum obj =
-    pure
-      $ obj
-          { x = obj.x + movementFactor * obj.vx
-          , y = obj.y + movementFactor * obj.vy
-          }
+  momentum obj = pure $ setObjPos obj (obj.position.x + movementFactor * obj.velocity.x) (obj.position.y + movementFactor * obj.velocity.y)
 
 -- TODO or else it catches its pray and stuff happens
 fishStaysInWaterOrElse :: Model -> Model
-fishStaysInWaterOrElse m =
-  m
-    { objects = Map.update (\v -> pure $ moveObjTo v v.x (waterLevel m)) "fish" m.objects
-    }
+fishStaysInWaterOrElse m = updateObjects m (\v -> pure $ setObjPos v v.position.x (waterLevel m)) "fish"
 
 -- controller for controlling?
 controller :: Model -> Effect Unit
 controller model = do
-  clearRect model.ctx { x: 0.0, y: 0.0, width: model.width, height: model.height }
-  render model.ctx $ drawObjects model.objects model.time
+  log $ "Tick " <> (show model.ticks) <> "; dTime " <> (show model.dTime)
+  -- if (mod model.ticks 10 == 0) then log $ "dTime " <> (show model.dTime) else pure unit
+  clearRect model.ctx { x: 0.0, y: 0.0, width: model.canvasSize.x, height: model.canvasSize.y }
+  render model.ctx $ drawObjects model.env model.time
 
 -- view updates
 mouseMoved :: CoordinatePair -> Model -> Model
 mouseMoved new_coord m =
   m
-    { pX = toNumber new_coord.x - m.baseX
-    , pY = toNumber new_coord.y - m.baseY
+    { pointerPos =
+      { x: toNumber new_coord.x - m.canvasPos.x
+      , y: toNumber new_coord.y - m.canvasPos.y
+      }
     }
 
--- sliderMoved :: Int -> Model -> Model
--- sliderMoved new_pos m =
---   m
---     { objects = Map.update (\v -> pure $ moveObjTo v (toNumber new_pos) v.y) "fish" m.objects
---     }
-timePassed :: Number -> Model -> Model
-timePassed new_time m =
-  m
-    { time = new_time
-    , dTime = new_time - m.time
-    }
+timePassed :: Number -> Number -> Int -> Model -> Model
+timePassed new_time old_time new_ticks m =
+    m
+      { time = new_time
+      , dTime = (new_time - old_time)
+      , ticks = new_ticks
+      }
 
 objectsInteract :: Model -> Model
 objectsInteract =
@@ -171,14 +169,15 @@ view model =
   state
     <$> lift animationFrame
     <*> lift mousePos
-    <*> intSlider_ 0 (floor model.width) (floor model.width)
+    -- <*> intSlider_ 0 (floor model.width) (floor model.width)
+
     -- apply to current model
 
     <*> pure model
   where
-  state time coord n =
+  state time coord =
     identity -- state changes
-      >>> timePassed time
+      >>> timePassed time (model.time + 0.0) (model.ticks + 1)
       >>> mouseMoved coord
       >>> objectsInteract
 
@@ -194,16 +193,14 @@ main = do
   let
     model =
       { ctx: ctx
-      , width: width
-      , height: height
-      , baseX: baseX
-      , baseY: baseY
-      , pX: baseX
-      , pY: baseY
-      , objects: Map.empty
+      , canvasSize: { x: width, y: height }
+      , canvasPos: { x: baseX, y: baseY }
+      , pointerPos: { x: baseX, y: baseY }
+      , env: { objects: Map.empty }
       , time: 0.0
       , dTime: 0.0
+      , ticks: 0
       }
   objects <- startObjects model
-  runFlareWith "controls" controller
-    $ view (model { objects = objects })
+  let modelPopulated = model { env { objects = objects } }
+  runFlareWith "controls" controller (view modelPopulated)
