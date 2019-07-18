d03b49b2860497e1ff5d23ac32ccf959d2aa5675
Radosław Cymer <cymerrad@gmail.com>
2019-07-18 18:55:41 +0200

    Calculating state only when page is visible

--- src/Main.purs
+++ src/Main.purs
@@ -4,41 +4,38 @@ import GameLogic
 import Models
 import Prelude
 
-import Color (hsl)
 import Data.Foldable (foldMap)
-import Data.Int (toNumber)
-import Data.Map as Map
-import Data.Maybe (Maybe)
-import Data.Tuple (Tuple(..), snd)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
-import MahUtils.Vector (Vector)
-import Signal (Signal, foldp, runSignal, sampleOn, (~>))
+import Signal (Signal, foldp, runSignal, sampleOn, (~>), (<~), constant, merge, filter, unwrap)
 import Signal.DOM (animationFrame, mousePos)
-import Signal.Time (Time, every)
+import Signal.Time (every)
 
 frameRateConst :: Number
-frameRateConst = 33.0
+frameRateConst = 100.0 -- 10fps
 
 frameRate :: Signal Number
 frameRate = every frameRateConst
 
+timeout :: Number
+timeout = 1000.0
+
 foreign import renderObject :: GameObject -> Effect Unit
 
-foreign import width :: Number
+foreign import isHidden :: Effect Boolean
 
-foreign import height :: Number
+foreign import width :: Effect Number
+
+foreign import height :: Effect Number
 
 drawObjects :: GameEnvironment -> Effect Unit
 drawObjects env = foldMap renderObject env.objects
 
 -- controller taking inputs on each frame and calculating new state with gameLogic
-controller :: Signal Input -> Signal Model
-controller input =
-  foldp gameLogic (initialModel width height)
-    (sampleOn mouseInp input)
-  where
-    mouseInp = map snd input -- perform calculations only when the pointer moves
+-- performs calculations only when the tickOn ticks
+controller :: Signal Input -> Signal _ -> Model -> Signal Model
+controller input tickOn initModel = foldp gameLogic initModel (sampleOn tickOn input)
 
 -- view draws the current model state
 view :: Model -> Effect Unit
@@ -48,9 +45,34 @@ main :: Effect Unit
 main = do
   frames <- animationFrame
   pos <- mousePos
-  log $ "width " <> (show width) <> " ;height " <> (show height)
+  w <- width
+  h <- height
+
+  signalIsHiddenAnimate <- unwrap (every frameRateConst ~> \_ -> isHidden)
+  signalIsHiddenSlow <- unwrap (every 2000.0 ~> \_ -> isHidden)
   let
+    -- visibilityChange = dropRepeats signalIsHidden
+    whenVisible = filter not true signalIsHiddenAnimate
+    whenVisibleSlow = filter not true signalIsHiddenSlow
+
     inputs :: Signal Input
     inputs = Tuple <$> frames <*> pos
-    scene = controller inputs
+
+    -- ticks when mouse moves or every 'frameRate' ms but only when visible
+    tickOn :: Signal _
+    tickOn = merge ((const true) <~ pos) whenVisible
+
+    scene :: Signal Model
+    scene = controller inputs tickOn $ initialModel w h
+
   runSignal $ scene ~> view
+  runSignal
+    $ ( sampleOn (whenVisibleSlow) $ (constant 1)
+          ~> ( \_ -> do
+                w_ <- width
+                h_ <- height
+                log $ "width " <> (show w_) <> " ;height " <> (show h_)
+                hidden <- isHidden
+                log $ "isHidden " <> (show hidden)
+            )
+      )
